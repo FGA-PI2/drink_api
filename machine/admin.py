2f19bfe68e870a369045e2b079faf4368f71a3f0
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-24 09:31:34 -0300

    heroku

--- machine/admin.py
+++ machine/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from models import * 
+from django.db.models.base import ModelBase
+
+
+class UserAdmin(admin.ModelAdmin):
+   search_fields = ('name',)    
+
+
+admin.site.register(User,UserAdmin)
