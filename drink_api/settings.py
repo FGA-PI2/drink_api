73d33b9e1eefde1d3011a557fdd915417310df26
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-23 23:04:57 -0300

    heroku

--- drink_api/settings.py
+++ drink_api/settings.py
@@ -27,7 +27,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False,cast=bool)
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
