57676c24807e77d01dfbb569d17f03edacc13a2f
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-23 22:57:21 -0300

    heroku

--- drink_api/settings.py
+++ drink_api/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 
 # Base-user
 AUTH_USER_MODEL = 'machine.User'
