beb742a1ed4190777b1bfe4b8586f03780b747f1
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-25 16:54:13 -0300

    update user manager

--- machine/managers.py
+++ machine/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password):
+    def _create_user(self, email, password):
         """
         Creates and saves a User with the given email and password.
         """
@@ -14,14 +14,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    # def create_user(self, email, password=None, **extra_fields):
-    #     extra_fields.setdefault('is_superuser', False)
-    #     return self._create_user(email, password, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
-    # def create_superuser(self, email, password, **extra_fields):
-    #     extra_fields.setdefault('is_superuser', True)
-    #
-    #     if extra_fields.get('is_superuser') is not True:
-    #         raise ValueError('Superuser must have is_superuser=True.')
-    #
-    #     return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
