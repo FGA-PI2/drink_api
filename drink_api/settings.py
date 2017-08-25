47f3f911ea6d87622bd1a23d9c36c6843b5e406e
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-24 22:59:51 -0300

    update db

--- drink_api/settings.py
+++ drink_api/settings.py
@@ -85,11 +85,9 @@ WSGI_APPLICATION = 'drink_api.wsgi.application'
 db_from_ev = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
+  'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
