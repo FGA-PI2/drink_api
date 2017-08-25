c1699765baebb496f5c6da2a1daffcda9e937057
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-08-25 14:36:46 -0300

    Update local db

--- drink_api/settings.py
+++ drink_api/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'drink_api.wsgi.application'
 db_from_ev = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
-  'default': dj_database_url.config(
-        default=config('HEROKU_POSTGRESQL_BRONZE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # this disables Cross domain requests
 CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL',default=True,cast=bool)
