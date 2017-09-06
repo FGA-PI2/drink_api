e356c1119529a2d064e80644b5f6007823bfad21
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-09-06 15:31:05 -0300

    update

--- administration/filters.py
+++ administration/filters.py
@@ -3,7 +3,7 @@ from rest_framework import filters
 from models import *
 
 class CompraFilter(filters.FilterSet):
-    data_compra = django_filters.DateTimeFilter(name="data_compra", lookup_expr='gte')
+    data_compra = django_filters.DateTimeFilter(name="data_compra", lookup_expr='exact')
 
     class Meta:
         model = Compra
