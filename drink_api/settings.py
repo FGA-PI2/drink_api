15a1afe5623958d3c659594c4604ba9cd2f7e641
Gustavo Sabino <gustavo.sabino@hotmail.com>
2017-12-03 13:57:26 -0200

    settings.py

--- drink_api/settings.py
+++ drink_api/settings.py
@@ -8,23 +8,25 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%2)&-x1v)$mtb158j8$%i59akupre8ao@6r4ulatwf%o4(hiba'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
-AUTH_USER_MODEL = 'authentication.User'
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -40,12 +42,9 @@ INSTALLED_APPS = [
     'authentication',
     'corsheaders',
     'django_filters',
-    'rest_framework.authtoken',
-    'paypal.standard.ipn'
+    'rest_framework.authtoken'
 
 ]
-
-PAYPAL_TEST = True
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -82,12 +81,21 @@ WSGI_APPLICATION = 'drink_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+db_from_ev = dj_database_url.config(conn_max_age=500)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+          default=config('DATABASE_URL')
+      )
 }
+
+# this disables Cross domain requests
+CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL',default=True,cast=bool)
+
+# this allows cookie being passed cross domain
+CORS_ALLOW_CREDENTIALS = config('CORS_ALLOW_CREDENTIALS',default=True,cast=bool)
+
 
 
 # Password validation
@@ -126,4 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Base-user
+AUTH_USER_MODEL = 'authentication.User'
